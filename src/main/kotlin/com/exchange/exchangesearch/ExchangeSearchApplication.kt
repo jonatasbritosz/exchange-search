66f7552456e6ec9eb95a52968cb9be01b77ccb90
Jonatas Brito <jonatas@tce.ma.gov.br>
2021-06-05 00:40:25 -0300

    first commit

---
+++ src/main/kotlin/com/exchange/exchangesearch/ExchangeSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.exchange.exchangesearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RateSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<RateSearchApplication>(*args)
+}
---
+++ src/test/kotlin/com/exchange/exchangesearch/ExchangeSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exchange.exchangesearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExchangeSearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
