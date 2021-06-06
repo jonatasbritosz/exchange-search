7e1da5d5bd7fae720b896419a340cac7e626a181
Jonatas Brito <jonatas@tce.ma.gov.br>
2021-06-06 13:46:23 -0300

    refactor: update application name

--- src/main/kotlin/com/exchange/exchangesearch/ExchangeSearchApplication.kt
+++ src/main/kotlin/com/exchange/exchangesearch/ExchangeSearchApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RateSearchApplication
+class ExchangeSearchApplication
 
 fun main(args: Array<String>) {
-	runApplication<RateSearchApplication>(*args)
+	runApplication<ExchangeSearchApplication>(*args)
 }
